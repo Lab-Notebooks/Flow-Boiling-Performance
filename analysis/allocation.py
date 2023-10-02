bc27820cdb5e466e327dfea89f3bdadb7dc57eb6
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-10-02 17:42:59 -0500

    update allocation

--- analysis/allocation.py
+++ analysis/allocation.py
@@ -12,7 +12,7 @@ def console(title, namespace):
     print(f'{" "*2}- {namespace.nxb*namespace.nyb*namespace.nzb*namespace.blocks} Points')
     print(f'{" "*2}- Resolution {namespace.resolution} mm')
     print(f'{" "*2}- Physical Time {namespace.simtime} ms')
-    print(f'{" "*2}- Wall Time {round(namespace.walltime,1)} hours')
+    print(f'{" "*2}- Wall Time {round(namespace.walltime_hours,1)} hours')
 
 length_scale_mm = 1
 time_scale_mm = 10
@@ -22,7 +22,7 @@ summit = SimpleNamespace(
     procs=1050,
     resolution=0.03 * length_scale_mm,
     simtime=2 * time_scale_mm,
-    walltime=8,
+    walltime_hours=8,
     nxb=16,
     nyb=16,
     nzb=16,
@@ -36,7 +36,7 @@ hpc3 = SimpleNamespace(
     procs=40,
     resolution=0.05 * length_scale_mm,
     simtime=1 * time_scale_mm,
-    walltime=(20000 / 60) / 60,
+    walltime_hours=(20000 / 60) / 60,
     nxb=10,
     nyb=10,
     nzb=10,
