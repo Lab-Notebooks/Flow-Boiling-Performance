f1729bbfb5728904558a11277e167bbdb55c5063
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-25 15:58:36 -0500

    Update Flash-X sha for 3D setups

--- simulation/FlowBoiling/Example3D/flashBuild.sh
+++ simulation/FlowBoiling/Example3D/flashBuild.sh
@@ -2,7 +2,7 @@
 NodeDir=$(realpath .)
 
 # run Flash-X setup
-cd $FLASHX_HOME && git checkout b46969dd && ./setup $FlashOptions
+cd $FLASHX_HOME && git checkout fa1e04b0 && ./setup $FlashOptions
 
 # compile the simulation and copy files
 cd $FLASHX_HOME/object && make -j && cp flashx $NodeDir/
