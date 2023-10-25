38eb3b92cd11317b2056c33ab14d5996c93bae5e
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-25 01:45:13 -0500

    Update configure and options for profiling code

--- simulation/FlowBoiling/Example2D/flashOptions.sh
+++ simulation/FlowBoiling/Example2D/flashOptions.sh
@@ -1,4 +1,8 @@
 # cache the value of current working directory
 
-FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 +amrex +parallelIO \
-   	      -site=$SiteHome SimForceInOut=True IOWriteGridFiles=True -2d -nxb=16 -nyb=16"
+FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 -2d -nxb=16 -nyb=16 \
+              +amrex +parallelIO -site=$SiteHome SimForceInOut=True IOWriteGridFiles=True"
+
+if [ $Profile = True ]; then
+	FlashOptions="$FlashOptions +hpctoolkit"
+fi
